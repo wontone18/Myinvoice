cea1a11fa22383eab3f49f53da4cd02ae8514264
SAJU <saju.g@wontonetech.com>
2019-11-10 18:57:23 +0530

    Updated

--- class/template.php
+++ class/template.php
@@ -1,10 +1,33 @@
 <?php
 
+/**
+ * 
+ * Template class
+ */
+
+require ('define/page-level-js.php');
+require ('define/page-level-css.php');
 
 class template{
 
+	function loadjs($action){
+
+		if(defined($action)){
+			return constant("js-".$action);
+		}
 
 
 
+	}
+
+
+	function loadcss($action){
+
+		if(defined($action)){
+			return constant("css-".$action);
+		}
+
+
+	}
 
 }
